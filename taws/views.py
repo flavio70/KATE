dc8e6c7da1d915dd221e35acc859613be02abc7d
colomx <colomx@it052016.it.alcatel-lucent.com>
2016-02-17 14:36:14 +0100

    export JIRA

--- taws/views.py
+++ taws/views.py
@@ -830,6 +830,7 @@ def collectReports(request):
 	from os.path import basename
 	from datetime import timedelta, datetime
 	from jenkinsapi.jenkins import Jenkins
+	from django.utils.safestring import mark_safe
 
 	context = RequestContext(request)
 	if 'login' not in request.session:
@@ -869,6 +870,7 @@ def collectReports(request):
 	buildMatrix=[]
 	counter=1
 	noteCounter=1
+	JIRAcsv=''
 	#for suites in root[0]:
 	for suites in root.findall(".suites/suite"):
 		if (suites.find('name').text.rfind('_main')>=0 or suites.find('name').text.rfind('_Main')>=0) and suites.find('name').text.rfind('EnvSettings')<0:
@@ -889,7 +891,7 @@ def collectReports(request):
 			for tps in root.findall(".suites/suite"):
 				if tps.find('name').text.rfind(testName)>=0 and tps.find('name').text.rfind('_main')<0 and tps.find('name').text.rfind('_Main')<0:
 					tpsTemp=tps.find('name').text.replace('(','').replace('.XML)','').replace(testName+'.','').split('_')
-					tpsName=tpsTemp[2].replace('-','.')
+					tpsName=tpsTemp[2]
 					tpsArea=tpsTemp[1]
 
 
@@ -918,10 +920,26 @@ def collectReports(request):
 
 					if azione == "addResult":
 						#myRecordSet.execute("INSERT INTO T_REPORT (SELECT '',"+str(id_pack)+",(SELECT * FROM T_TPS join T_DOMAIN on(id_domain=T_DOMAIN_id_domain) join T_AREA on(id_area=T_AREA_id_area ) where tps_reference='"+tpsName+"' and area_name='"+tpsArea+"'),'"+errMsg+"','"+tpsTestStatus+"','"+request.POST.get('note'+str(noteCounter),"NA")+"')")
-						myRecordSet.execute("INSERT INTO T_REPORT (SELECT null,"+str(id_pack)+",id_tps,"+str(id_run)+",'"+errMsg+"','"+tpsTestStatus+"','"+request.POST.get('note'+str(noteCounter),"NA")+"','"+request.session['login']+"','' FROM T_TPS join T_TEST_REVS on(id_TestRev=T_TEST_REVS_id_TestRev) join T_AREA on(id_area=T_AREA_id_area ) join T_PROD on(T_PROD_id_prod=id_prod) join T_TEST_REVS on(id_TestREv=T_TEST_REV_id_TestRev) where tps_reference='"+tpsName+"' and area_name='"+tpsArea+"' and id_TestRev="+testID+")")
+						myRecordSet.execute("INSERT INTO T_REPORT (SELECT null,"+str(id_pack)+",id_tps,"+str(id_run)+",'"+errMsg+"','"+tpsTestStatus+"','"+request.POST.get('note'+str(noteCounter),"NA")+"','"+request.session['login']+"','' FROM T_TPS join T_TEST_REVS on(id_TestRev=T_TEST_REVS_id_TestRev) join T_DOMAIN on(T_DOMAIN_id_domain=id_domain) join T_AREA on(id_area=T_AREA_id_area ) join T_PROD on(T_PROD_id_prod=id_prod) where tps_reference='"+tpsName+"' and area_name='"+tpsArea+"' and id_TestRev="+testID+")")
 						#nodeType="INSERT INTO T_REPORT (SELECT '',"+str(id_pack)+",(SELECT * FROM T_TPS join T_DOMAIN on(id_domain=T_DOMAIN_id_domain) join T_AREA on(id_area=T_AREA_id_area ) where tps_reference='"+tpsName+"' and area_name='"+tpsArea+"'),'"+errMsg+"','"+tpsTestStatus+"','"+request.POST.get('note'+str(noteCounter),"NA")+"')"
 						#myRecordSet.execute("select *,if(T_EQUIPMENT_id_equipment is null,'NA',T_EQUIPMENT_id_equipment) as checkNode,T_EQUIPMENT.name as nodeName,T_EQUIP_TYPE.name as nodeType,T_PACKAGES.label_ref as nodeSWP,T_RUNTIME.owner as suiteOwner from T_RUNTIME left join T_RTM_BODY on(id_run=T_RUNTIME_id_run) join T_EQUIPMENT on(id_equipment=T_EQUIPMENT_id_equipment) join T_EQUIP_TYPE on(id_type=T_EQUIP_TYPE_id_type) join T_PACKAGES on(id_pack=T_RTM_BODY.T_PACKAGES_id_pack)  where job_name='"+job_name+"' and job_iteration="+str(buildId))
 						dbConnection.commit()
+
+					if azione == "exportJIRA":
+						userJIRA='ptnswtaut'
+						pwdJIRA='ptnswtaut'
+						zq_comment = nodeSWP+" - "+nodeType+" - ("+tpsTestStatus+")"
+						myRecordSet.execute("SELECT story_reference FROM T_TPS join T_TEST_REVS on(id_TestRev=T_TEST_REVS_id_TestRev) join T_DOMAIN on(T_DOMAIN_id_domain=id_domain) join T_AREA on(id_area=T_AREA_id_area ) join T_PROD on(T_PROD_id_prod=id_prod) join T_JIRA_STORY using(T_DOMAIN_id_domain) where tps_reference='"+tpsName+"' and area_name='"+tpsArea+"' and id_TestRev="+testID)
+						row=myRecordSet.fetchone()
+						JIRAcsv+="call jira -s \"http://sts.app.alcatel-lucent.com\" -u \""+userJIRA+"\" -p \""+pwdJIRA+"\" -a runFromIssueList --common \"--action progressIssue --issue @issue@ --step \\\"Re Run\\\"\" --search \"project=PTNSW and issuetype=\\\"Test Case Sub-Task\\\" and issuefunction in subtasksOf(\\\"issueKey="+row['story_reference']+"\\\") and status!= \\\""+tpsTestStatus+"\\\" and summary ~ \\\""+tpsName+" \\\"\"\\n"
+						if tpsTestStatus == 'Failed':
+							JIRAcsv+="call jira -s \"http://sts.app.alcatel-lucent.com\" -u \""+userJIRA+"\" -p \""+pwdJIRA+"\" -a runFromIssueList --common \"--action linkIssue --issue @issue@ --toIssue \\\""+Tstr+"\\\" --link \\\"Related\\\"\" --search \"project=PTNSW and issuetype=\\\"Test Case Sub-Task\\\" and issuefunction in subtasksOf(\\\"issueKey="+row['story_reference']+"\\\") and status!= \\\""+tpsTestStatus+"\\\" and summary ~ \\\""+tpsName+" \\\"\"\\n"
+						JIRAcsv+="call jira -s \"http://sts.app.alcatel-lucent.com\" -u \""+userJIRA+"\" -p \""+pwdJIRA+"\" -a runFromIssueList --common \"--action progressIssue --issue @issue@ --step \\\""+tpsTestStatus+"\\\"\" --search \"project=PTNSW and issuetype=\\\"Test Case Sub-Task\\\" and issuefunction in subtasksOf(\\\"issueKey="+row['story_reference']+"\\\") and status!= \\\""+tpsTestStatus+"\\\" and summary ~ \\\""+tpsName+" \\\"\"\\n"
+						JIRAcsv+="call jira -s \"http://sts.app.alcatel-lucent.com\" -u \""+userJIRA+"\" -p \""+pwdJIRA+"\" -a runFromIssueList --common \"--action addComment --issue @issue@ --comment \\\""+zq_comment+"\\\"\" --search \"project=PTNSW and issuetype=\\\"Test Case Sub-Task\\\" and issuefunction in subtasksOf(\\\"issueKey="+row['story_reference']+"\\\") and summary ~ \\\""+tpsTestStatus+" \\\"\"\\n"
+
+
+
+
 					tpsList.append({'nodeName':nodeName,'nodeType':nodeType,'nodeSWP':nodeSWP,'noteCounter':noteCounter,'tpsName':tpsName,'tpsArea':tpsArea,'tpsBgcolor':tpsBgcolor,'tpsFontcolor':tpsFontcolor})
 					noteCounter+=1
 			buildMatrix.append({'bgcolor':bgcolor,
@@ -945,6 +963,7 @@ def collectReports(request):
 		'azione':azione,
 		'KateDB':KateDB,
 		'instance': str(buildId),
+		'JIRAcsv':mark_safe(JIRAcsv),
 		'buildMatrix':buildMatrix}
 	return render(request,'taws/collectReports.html',context_dict)
 
@@ -1169,7 +1188,7 @@ def add_bench(request):
 
 
 
-	if bench != 'NONE':
+	if bench != 'NONE' and bench != '':
 		SQL="SELECT *,T_NET.IP as benchIP,T_NET.NM as benchNM,T_NET.GW as benchGW,T_EQUIPMENT.description as benchDescription,T_EQUIPMENT.note as benchNote,group_concat(concat(ip1.ip,'#',port,'#',if(slot is null,'-',slot),'#',if(subslot is null,'-',subslot)) separator '|') as serials,T_SCOPE.description as scope,T_EQUIP_TYPE.name as type,T_EQUIPMENT.name as benchName,if(status like '%ING%',status,'IDLE') as benchStatus,T_EQUIPMENT.owner as reference,runtime.owner as author FROM T_EQUIPMENT LEFT JOIN (select * from T_RTM_BODY left join T_RUNTIME on(id_run=T_RUNTIME_id_run) where status='RUNNING') as runtime on(id_equipment=T_EQUIPMENT_id_equipment) left join T_EQUIP_TYPE on(id_type=T_EQUIP_TYPE_id_type) left join T_NET on(T_NET.T_EQUIPMENT_id_equipment=id_equipment) LEFT JOIN T_LOCATION on(T_LOCATION_id_location=id_location) LEFT JOIN T_SERIAL on(T_SERIAL.T_EQUIPMENT_id_equipment=id_equipment) LEFT JOIN T_SCOPE on(T_SCOPE_id_scope=id_scope) LEFT JOIN T_NET as ip1 on(T_SERIAL.T_NET_id_ip=ip1.id_ip) where id_equipment="+str(bench)+" group by id_equipment"
 		#SQL="SELECT * from T_EQUIPMENT join T_NET on(id_equipment=T_EQUIPMENT_id_equipment) where id_equipment="+str(bench)
 		myRecordSet.execute(SQL)
