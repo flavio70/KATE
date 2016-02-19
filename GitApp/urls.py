e4f92d56d03a455b813435c0153a190187904c20
ippolf <ippolf@IT052016.it.alcatel-lucent.com>
2016-02-19 12:01:37 +0100

    end of DB hook management

--- GitApp/urls.py
+++ GitApp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'^gitlab_webhook/$', views.gitlab_webhook, name='gitlab_webhook'),
 	url(r'^getgittag/$', views.getgittag),
-  url(r'^setGitFlag/$',views.setGitFlag),
+	url(r'^setGitFlag/$',views.setGitFlag),
+	url(r'^gitTagShow/$',views.gitTagShow),
 ]
